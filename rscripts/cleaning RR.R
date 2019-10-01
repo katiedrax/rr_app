eb22f530458d45645f61312677ddb9429648609a
katiedrax <45069850+katiedrax@users.noreply.github.com>
2019-10-01 19:56:37 +0100

    changed and pasted into app

--- rscripts/cleaning RR.R
+++ rscripts/cleaning RR.R
@@ -84,3 +84,42 @@ tail(df)
 df <- lapply(df[, 2:22], as.factor)
 table(df$X5..Offered.for.replication.studies)
 prop.table(df, 1)
+
+
+
+ 
+       
+       
+       # remove all rows without a journal
+       
+       df <- df[!is.na(df$Journal),]
+       
+       # replace tick
+       
+       
+       # replace NA with no
+       
+       df[is.na(df)] <- "No"
+       
+       # clean headers
+       
+       colnames(df) <- gsub("X[0-9]\\..|X[0-9][0-9]\\..","", colnames(df))
+       
+       # subset rows that do not have pre-study peer review OR don't offer IPA
+       not_rr <- subset(df, 
+                        (is.na(df[, "Includes.pre.study.peer.review"])) | (is.na(df[, "Offers.provisional.pre.study.acceptance"])))
+       
+       # subset rows that have pre-study peer review AND IPA
+       
+       rr <- subset(df, 
+                    (!is.na(df[, "Includes.pre.study.peer.review"])) & (!is.na(df[, "Offers.provisional.pre.study.acceptance"])))
+       
+       ##############
+       #### define input ####
+       ###############
+       
+       observe({
+         #define input as variables in rr
+         updateSelectInput(session, "vars", choices = colnames(rr))
+       })
+       
